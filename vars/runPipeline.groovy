9fe8fbe81d9edd1a18b0fc59de95a62ee828572a
Prakash Mohan <prakash.mohanm@gmail.com>
2019-11-01 15:26:05 +0000

    Update runPipeline.groovy

--- vars/runPipeline.groovy
+++ vars/runPipeline.groovy
@@ -1,27 +1,28 @@
 #!/usr/bin/env groovy
-def call(){
-  node {
-     def mvnHome
-     stage('Preparation') { // for display purposes
-        // Get some code from a GitHub repository
-        //git 'https://github.com/jglick/simple-maven-project-with-tests.git'
-        // Get the Maven tool.
-        // ** NOTE: This 'M3' Maven tool must be configured
-        // **       in the global configuration.           
-        mvnHome = tool 'M3'
-     }
-     stage('Build') {
-        // Run the maven build
-        withEnv(["MVN_HOME=$mvnHome"]) {
-           if (isUnix()) {
-              sh '"$MVN_HOME/bin/mvn" -Dmaven.test.failure.ignore clean package'
-           } else {
-              bat(/"%MVN_HOME%\bin\mvn" -Dmaven.test.failure.ignore clean package/)
-           }
+def call() {
+    node {
+        def mvnHome
+        stage('Preparation') { // for display purposes
+            // Get some code from a GitHub repository
+            //git 'https://github.com/jglick/simple-maven-project-with-tests.git'
+            // Get the Maven tool.
+            // ** NOTE: This 'M3' Maven tool must be configured
+            // **       in the global configuration.           
+            mvnHome = tool 'M3'
         }
-     }
-     stage('Results') {
-        junit '**/target/surefire-reports/TEST-*.xml'
-        archiveArtifacts 'target/*.jar'
-     }
-  }
+        stage('Build') {
+            // Run the maven build
+            withEnv(["MVN_HOME=$mvnHome"]) {
+                if (isUnix()) {
+                    sh '"$MVN_HOME/bin/mvn" -Dmaven.test.failure.ignore clean package'
+                } else {
+                    bat(/"%MVN_HOME%\bin\mvn" -Dmaven.test.failure.ignore clean package/)
+                }
+            }
+        }
+        stage('Results') {
+            junit '**/target/surefire-reports/TEST-*.xml'
+            archiveArtifacts 'target/*.jar'
+        }
+    }
+}
